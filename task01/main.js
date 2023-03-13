718d0f5934601b29a34c42587b49300ed8961cd4
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-03-13 13:33:59 +0200

    solved tasks 01-03

---
+++ task01/main.js
@@ -0,0 +1,20 @@
+/*//task 01
+// на HTML-сторінці додаємо наступний скрипт
+<script>
+        alert('Nataliia');
+      </script>
+
+//task 02
+console.log('Voronchak');*/
+
+//task 03
+const myFavoriteSport = "tennis";
+let myFavoriteSeason = "summer";
+
+console.log(myFavoriteSport);
+console.log(myFavoriteSeason);
+
+myFavoriteSeason = myFavoriteSport;
+
+console.log(myFavoriteSport);
+console.log(myFavoriteSeason);
