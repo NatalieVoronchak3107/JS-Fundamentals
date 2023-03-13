c088591b349b564c08f410df978beb7e46570b51
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-03-13 13:43:53 +0200

    solved task 04

--- task01/main.js
+++ task01/main.js
@@ -1,4 +1,4 @@
-//task 01
+/task 01
 // на HTML-сторінці додаємо наступний скрипт
 <script>
         alert('Nataliia');
@@ -18,3 +18,14 @@ myFavoriteSeason = myFavoriteSport;
 
 console.log(myFavoriteSport);
 console.log(myFavoriteSeason);
+
+//task 04
+const myObject = {
+    String: 'Greetings from Nataliia',
+    Number: 7,
+    Boolen: true,
+    Undefined: undefined,
+    Null: null
+  };
+
+  console.log(myObject);
