734467c6f836f138afe1f29f2c72cbd9e0f28170
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-03-13 12:37:41 +0200

    solved task 05

--- task02/main.js
+++ task02/main.js
@@ -1,12 +1,13 @@
+/*
 //task 01
 let a = 3;
 let b =  4;
 let c = 5;
 
 if (a < b || b < c) {
-  console.log('The statement is accurate');
+  console.log("The statement is accurate");
 } else {
-  console.log('The statement is unvalid');
+  console.log("The statement is unvalid");
 }
 
 //task 02
@@ -65,3 +66,34 @@ for (let i = arr.length - 1; i >= 0; i--) {
 console.log(data); // 5
 console.log(arr); // [4, 2, 1, 6, 3, 2]
 console.log(newArr); // [5]
+*/
+//task 05
+const a = prompt("Enter the length of side 'a'");
+const b = prompt("Enter the length of side 'b'");
+const c = prompt("Enter the length of side 'c'");
+
+// Перевіряємо чи коректні введені дані
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+  console.log('Incorrect data');
+} else {
+  // Обчислюємо півпериметр
+  const p = (a + b + c) / 2;
+
+  // Обчислюємо площу за формулою Герона
+  const area = Math.sqrt(p * (p - a) * (p - b) * (p - c)).toFixed(3);
+
+  // Виводимо площу трикутника
+  console.log(`The area of triangle is equal to ${area}`);
+
+  // Перевіряємо чи трикутник є прямокутним за теоремою Піфагора
+  const isRightTriangle = a * a + b * b === c * c ||
+    a * a + c * c === b * b ||
+    b * b + c * c === a * a;
+
+  // Виводимо результат перевірки
+  if (isRightTriangle) {
+    console.log('This is a right-angled triangle');
+  } else {
+    console.log('This is not a right-angled triangle');
+  }
+}
