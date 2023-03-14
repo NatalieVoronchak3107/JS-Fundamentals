225049f8a9eb6bf93dd646a110c5fc1787867522
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-03-14 14:05:01 +0200

    revised js

--- assignment01/additional-task/main.js
+++
@@ -1,7 +0,0 @@
-//solution 01
-const ticket = prompt('Entered number of Your ticket:', '');
-console.log(ticket);
-console.log(typeof ticket);
-const ticketNum = ticket;
-console.log(ticketNum);
-console.log(typeof ticketNum);
