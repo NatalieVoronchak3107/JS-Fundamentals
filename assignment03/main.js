7792405d5af58daeeea36565de93b0df217fdddf
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-03-20 14:45:19 +0200

    additional task done

--- assignment03/main.js
+++ assignment03/main.js
@@ -125,3 +125,17 @@ function findUnique(arr) {
 console.log(findUnique([1, 2, 3, 5, 3]))
 console.log(findUnique([1, 2, 3, 5, 11]));  
 
+
+/*(Ускладнене. Задача не оцінюється. Для тих, кому хочеться поробити ще щось)
+Створити функцію create() , яка приймає один аргумент у вигляді рядка. Ця функція повертає анонімну функцію, яка перевіряє чи переданий в неї аргумент 
+збігається з аргументом зовнішньої функції. */
+
+function create(password) {
+  return function(input) {
+    return input === password;
+  }
+}
+
+const tom = create("pass_for_Tom");
+console.log(tom("pass_for_Tom"));
+console.log(tom("pass_for_tom")); 
