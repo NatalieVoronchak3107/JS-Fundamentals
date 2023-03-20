ff98db658777000939e47d9143e3142a60be6b34
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-03-20 13:55:48 +0200

    revised task 07

--- assignment03/main.js
+++ assignment03/main.js
@@ -112,7 +112,6 @@ findUnique([1, 2, 3, 5, 3]);  // => false
 findUnique([1, 2, 3, 5, 11]); // => true */
 
 function findUnique(arr) {
-
     for (let i = 0; i < arr.length; i++) {
       for(let j = i + 1; j < arr.length; j++) {
         if (arr[i] === arr[j]) {
