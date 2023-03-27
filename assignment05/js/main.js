16eb2e49a2c805670d43b32b408eb59ae0412b21
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-03-27 11:26:01 +0300

    task 03 done

--- assignment05/js/main.js
+++ assignment05/js/main.js
@@ -81,6 +81,39 @@ const stud1 = new Student("Petro", "Petrenko", 2019);
 console.log(stud1.showFullName("Petrovych")); // Petrenko Petro Petrovych
 console.log("Current course: " + stud1.showCourse()); //Current course: 4 */
 
+class Person {
+  constructor(name, surname) {
+    this.name = name;
+    this.surname = surname;
+  }
+
+  getFullName() {
+    return `${this.surname} ${this.name}`;
+  }
+}
+
+class Student extends Person {
+  constructor(name, surname, year) {
+    super(name, surname);
+    this.year = year;
+  }
+
+  getFullName(middleName) {
+    return `${this.surname} ${this.name} ${middleName}`;
+  }
+
+  showCourse() {
+    const currentYear = new Date().getFullYear();
+    const course = currentYear - this.year + 1;
+    return course >= 1 && course <= 6 ? course : "Unknown course";
+  }
+}
+
+const stud1 = new Student("Petro", "Petrenko", 2019);
+
+console.log(stud1.getFullName("Petrovych"));
+console.log("Current course: ", stud1.showCourse());
+
 /* 4. 
 А. Реалізувати клас, який описує простий маркер. У класі мають бути такі компоненти:
 - поле, яке зберігає колір маркера;
