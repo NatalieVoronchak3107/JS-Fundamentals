20eeeb630a5bc2edb382a5be0d8b0c0dbdcd1df2
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-04-07 13:48:59 +0300

    task 04 done

--- assignment09/main.js
+++ assignment09/main.js
@@ -30,3 +30,18 @@ $(function () {
 });
 
 /*4. На HTML-сторінці є 6 чекбоксів. Напишіть скріпт, який після того, як користувач позначив будь-які 3 чекбокси, всі чекбокси робить неактивними. */
+
+$(function () {
+  let checkboxCount = 0;
+
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(":checked")) {
+      checkboxCount++;
+    } else {
+      checkboxCount--;
+    }
+    if (checkboxCount >= 3) {
+      $('input[type="checkbox"]').attr("disabled", "disabled");
+    }
+  });
+});
