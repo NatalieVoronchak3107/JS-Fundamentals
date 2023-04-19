2b787bdd78f34c54106e0d9eb734bb5306393d8c
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-04-19 11:24:45 +0300

    task 02 done

--- assignment11/main.js
+++ assignment11/main.js
@@ -27,6 +27,25 @@ calcArrProduct([3,4,5]).then(result => console.log(result)); // 60
 calcArrProduct([5,"user2", 7, 12]).then(result => console.log(result));
 // "Error! Incorrect array!" */
 
+function calcArrProduct(arr) {
+    return new Promise(function (resolve, reject) {
+        let product = 1;
+        for (let i = 0; i < arr.length; i++) {
+            if (typeof arr[i] === 'number') {
+                product *= arr[i];
+            } else {
+                reject('Error! Incorrect array!');
+                return;
+            }
+        }
+        resolve(product);
+    });
+}
+
+calcArrProduct([3, 4, 5]).then(result => console.log(result)); 
+calcArrProduct([5, "user2", 7, 12]).catch(result => console.log(result));
+
+
 
 
 /* 3. Створіть наступний асинхронний ланцюжок promise:
